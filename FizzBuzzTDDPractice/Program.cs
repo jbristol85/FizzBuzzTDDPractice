32390da1599d0a8f8b51bf07cb9d988ac8461503
jbristol85 <josh.bristol2@gmail.com>
2018-07-24 01:26:13 -0400

    Test Driven Development Practice - FizzBuzz

--- FizzBuzzTDDPractice/Program.cs
+++ FizzBuzzTDDPractice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FizzBuzzLibrary;
 
 namespace FizzBuzzTDDPractice
 {
@@ -10,6 +11,12 @@ namespace FizzBuzzTDDPractice
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzzer.GetValue(i));
+            }
+
+            Console.ReadLine();
         }
     }
 }
